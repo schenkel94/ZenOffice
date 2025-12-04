1fb961e01b6ebcf28cb8fe410858076d0a88b516
schenkel94 <mario.silveira94@hotmail.com>
2025-12-04 14:14:12 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,25 @@
-const CACHE_NAME = 'zenoffice-v1';
+const CACHE_NAME = 'zenoffice-master-v4';
 const urlsToCache = [
   './',
   './index.html',
   './manifest.json',
   './icon.png',
-  './qr.png'
+  './qr.png',
+  './profile.png',
+  './alarme.mp3',
+  './bubble.mp3',
+  './foco.mp3',
+  './natureza.mp3',
+  './lo-fi.mp3'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then(cache => {
+        console.log('Cache aberto: Baixando todos os arquivos...');
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
@@ -18,10 +27,28 @@ self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {
+        // Retorna do cache se existir
         if (response) {
           return response;
         }
+        // Se não, busca na rede
         return fetch(event.request);
       })
+  );
+});
+
+// Remove caches antigos ao atualizar a versão
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
